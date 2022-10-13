424ff703410c0baab05ef9c289eff3dec636e09d
Clodagh MURPHY <clmurphy@e3r3p21.clusters.42paris.fr>
2022-10-13 16:45:30 +0200

    iterating

--- include/container.hpp
+++ include/container.hpp
@@ -10,4 +10,18 @@
 # include <exception>
 # include <stdexcept>
 
+namespace ft{
+    template< bool B, class T = void >
+        struct enable_if {};
+
+    template<bool B>
+        struct bool_type {
+            static const bool = B;
+        };
+
+    typedef bool_type<true>     true_type;
+    typedef bool_type<false> false_type;
+
+}
+
 # endif
